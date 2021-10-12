395baa227f66e6286f9b8700b1481bc42afeb3c5
Laura Court <laura.mcourt@gmail.com>
2021-10-12 00:12:33 -0400

    Add user logging in/out
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back, #{user.email}!"
+      redirect_to dashboard_path(user)
+    else
+      flash[:danger] = 'Invalid email or password.'
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
