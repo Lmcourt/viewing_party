153095fe3060032774be15c0a97a10cc381d8106
Laura Court <laura.mcourt@gmail.com>
2021-10-11 21:55:21 -0400

    Add routes for welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
+
+  resources :sessions, only: :create
+  get '/login', controller: :sessions, action: :new
+  get '/logout', controller: :sessions, action: :destroy
+
+  resource :users, only: :create
+  get '/profile', controller: :users, action: :show
+  get '/register', controller: :users, action: :new
 end
