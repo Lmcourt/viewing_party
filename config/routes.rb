c37ef55ce8b991ff3ce8cf3345a1cdf15bd9a1a7
Laura Court <laura.mcourt@gmail.com>
2021-10-12 00:12:49 -0400

    Change verb from get to delete for logout
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: :create
   get '/login', controller: :sessions, action: :new
-  get '/logout', controller: :sessions, action: :destroy
+  delete '/logout', controller: :sessions, action: :destroy
 
   resource :users, only: :create
   get '/dashboard', controller: :users, action: :show
