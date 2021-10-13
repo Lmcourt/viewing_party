663daa8ceaed4958332ce8445a7829c506550c94
Laura Court <laura.mcourt@gmail.com>
2021-10-13 16:46:37 -0400

    Add cast/review/specific movie service
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -8,14 +8,30 @@ class MovieService
       end
     end
 
+    def specific_movie(movie_id)
+      MovieClient.fetch("/3/movie/#{movie_id}?")
+    end
+
+    def cast(movie_id)
+      MovieClient.fetch("/3/movie/#{movie_id}/credits?")[:cast]
+    end
+
+    def reviews(movie_id)
+      MovieClient.fetch("/3/movie/#{movie_id}/reviews?")[:results]
+    end
+
     private
 
     def top_movies
-      MovieClient.fetch('/3/discover/movie?language=en-US&sort_by=vote_average.desc')
+      page_1 = MovieClient.fetch('/3/discover/movie?language=en-US&sort_by=vote_average.desc&page=1')
+      page_2 = MovieClient.fetch('/3/discover/movie?language=en-US&sort_by=vote_average.desc&page=2')
+      page_1[:results] + page_2[:results]
     end
 
     def search_movies(title)
-      MovieClient.fetch("/3/search/movie?&query=#{title}&page=1")
+      page_2 = MovieClient.fetch("/3/search/movie?&query=#{title}&page=1")
+      page_1 = MovieClient.fetch("/3/search/movie?&query=#{title}&page=2")
+      page_1[:results] + page_2[:results]
     end
   end
 end
