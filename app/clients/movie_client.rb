153ec3c516ce3f4a2f620800d1b3ef4b4cdcfb1f
Laura Court <laura.mcourt@gmail.com>
2021-10-13 16:56:29 -0400

    Remove .take(40) from fetch method
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/clients/movie_client.rb
+++ app/clients/movie_client.rb
@@ -2,8 +2,10 @@ class MovieClient
   class << self
     def fetch(url)
       url += "&api_key=#{ENV['movie_api_key']}"
-      parse_data(conn.get(url)).take(40)
+      parse_data(conn.get(url))
     end
+
+    private
 
     def parse_data(response)
       JSON.parse(response.body, symbolize_names: true)
