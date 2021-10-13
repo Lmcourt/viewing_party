fd9833c041439596d4c11e00d9fb1b5776c5434b
Laura Court <laura.mcourt@gmail.com>
2021-10-13 16:55:35 -0400

    Add movie strong params
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,12 @@ class MoviesController < ApplicationController
   before_action :require_user
 
   def index
-    @movies = MovieFacade.movies(params)
+    @movies = MovieFacade.movies(movie_params)
+  end
+
+  private
+
+  def movie_params
+    params.permit(:top, :title)
   end
 end
