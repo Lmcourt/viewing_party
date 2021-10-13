faffef8d5f37a5c3ce3e2eac69e70a87e8ec60bc
Laura Court <laura.mcourt@gmail.com>
2021-10-13 16:49:26 -0400

    Add movie factory
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

---
+++ spec/factories/movies_poro.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :movies_poro do
+    title { Faker::Movie.title }
+    vote_average { Faker::Number.within(range: 1..10) }
+    id { Faker::Number.within(range: 1..10) }
+
+    initialize_with { new(attributes) }
+  end
+end
