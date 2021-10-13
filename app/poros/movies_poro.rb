b6e0cce8f9ceebfdc72d58186be6233a407f544b
Laura Court <laura.mcourt@gmail.com>
2021-10-13 16:44:54 -0400

    Rename MoviePoro to MoviesPoro
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

---
+++ app/poros/movies_poro.rb
@@ -0,0 +1,9 @@
+class MoviesPoro
+  attr_reader :title, :vote_average, :id
+
+  def initialize(data)
+    @id = data[:id]
+    @title = data[:title]
+    @vote_average = data[:vote_average]
+  end
+end
