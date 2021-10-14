7502ca495d168cd68a60977bfcb0fc0da32d887b
Laura Court <laura.mcourt@gmail.com>
2021-10-14 17:22:37 -0400

    Add hosted parties scope
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,11 @@
 class Party < ApplicationRecord
   has_many :invitations, dependent: :destroy
   has_many :users, through: :invitations
+
+  scope :hosted, ->(user_id) {
+    joins(:invitations)
+      .merge(Invitation.host)
+      .merge(Invitation.find_host(user_id))
+      .group(:id)
+  }
 end
