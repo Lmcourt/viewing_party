294459a9fc6c96ebd4bafe9fa61100ccd9a3df0b
Laura Court <laura.mcourt@gmail.com>
2021-10-13 21:55:28 -0400

    Remove exist test
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- spec/poros/movie_poro_spec.rb
+++ spec/poros/movie_poro_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 RSpec.describe MoviesPoro do
   let(:movie) { MoviesPoro.new({ id: 1, title: 'Movie', vote_average: 1 }) }
 
-  it 'exists' do
-    expect(movie).to be_a(MoviesPoro)
-  end
-
   it 'has attributes' do
     expect(movie.title).to eq('Movie')
     expect(movie.vote_average).to eq(1)
