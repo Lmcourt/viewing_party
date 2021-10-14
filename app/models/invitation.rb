be467771d06be3cef9c0304d1627e6e6015a48c1
Laura Court <laura.mcourt@gmail.com>
2021-10-14 17:22:23 -0400

    Add create and find host scope
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,4 +3,12 @@ class Invitation < ApplicationRecord
   belongs_to :party
 
   enum role: %w[attendee host]
+
+  scope :create_host, ->(host_id) {
+    find_host(host_id).first.update_attribute(:role, 1)
+  }
+
+  scope :find_host, ->(user_id) {
+    where(user_id: user_id)
+  }
 end
