eb45116cef937fe757ec465c1ec87aa549db6a67
Laura Court <laura.mcourt@gmail.com>
2021-10-14 17:22:11 -0400

    Test scope creating and finding host
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -8,11 +8,34 @@ RSpec.describe Invitation do
 
   describe 'roles' do
     let(:roles) { %w[attendee host] }
-    
+
     it 'has roles' do
       Invitation.roles.keys.each_with_index do |role, i|
         expect(role).to eq(roles[i])
       end
+    end
+  end
+
+  describe 'scopes' do
+    let!(:users) { create_list :user, 5 }
+    let!(:party) { create :party }
+
+    before :each do
+      party.users << users
+    end
+
+    it 'creates a host' do
+      party.invitations.create_host(users.first.id)
+      result = party.invitations.where(role: 1)
+      expect(result.size).to eq(1)
+      expect(result.first.user_id).to eq(users.first.id)
+    end
+
+    it 'finds a host' do
+      party.invitations.create_host(users.last.id)
+      result = Invitation.find_host(users.last.id)
+      
+      expect(result).to eq(users.last.invitations)
     end
   end
 end
