47a25550cb047d8c7cd816b66263822e918d5267
Laura Court <laura.mcourt@gmail.com>
2021-10-14 13:17:28 -0400

    Add id attribute
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/poros/specific_movie_poro.rb
+++ app/poros/specific_movie_poro.rb
@@ -1,7 +1,8 @@
 class SpecificMoviePoro
-  attr_reader :title, :vote_average, :runtime, :genres, :overview, :cast, :reviews
+  attr_reader :id, :title, :vote_average, :runtime, :genres, :overview, :cast, :reviews
 
   def initialize(data)
+    @id = data[:id]
     @title = data[:title]
     @vote_average = data[:vote_average]
     @runtime = data[:runtime].to_i
