b13598fe19b8348b6cf33e259db8e672c4fc6d5a
Laura Court <laura.mcourt@gmail.com>
2021-10-14 19:38:39 -0400

    Test hosted/invited parties section
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -29,5 +29,62 @@ RSpec.describe 'User show' do
         expect(current_path).to eq(discover_path)
       end
     end
+
+    describe 'parties section' do
+      let!(:user) { create :user }
+      let!(:users) { create_list :user, 10 }
+      let!(:parties) { create_list :party, 7 }
+      let!(:hosted_parties) { [parties[1], parties[3], parties[6]] }
+      let!(:invited_parties) { parties - hosted_parties }
+
+      before :each do
+        users << user
+        parties.each do |party|
+          party.users << users
+          if !hosted_parties.include?(party)
+            party.invitations.create_host(users[0..-2].sample.id)
+          end
+        end
+
+        hosted_parties.each do |party|
+          party.invitations.create_host(user.id)
+        end
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user)
+          .and_return(user)
+
+        visit dashboard_path
+      end
+
+      it 'has users hosted parties' do
+        within('#hosted-parties') do
+          hosted_parties.each do |party|
+            within("#party-#{party.id}") do
+              expect(page).to have_link(party.movie_title)
+              expect(page).to have_content(party.formatted_duration)
+              expect(page).to have_content(party.date.strftime('%b %-d, %Y'))
+              expect(page).to have_content(party.start_time.strftime('%I:%M %P'))
+              expect(page).to have_content('Hosting')
+              expect(page).to_not have_content('Invited')
+            end
+          end
+        end
+      end
+
+      it 'has users invited parties' do
+        within('#invited-parties') do
+          invited_parties.each do |party|
+            within("#party-#{party.id}") do
+              expect(page).to have_link(party.movie_title)
+              expect(page).to have_content(party.formatted_duration)
+              expect(page).to have_content(party.date.strftime('%b %-d, %Y'))
+              expect(page).to have_content(party.start_time.strftime('%I:%M %P'))
+              expect(page).to have_content('Invited')
+              expect(page).to_not have_content('Hosting')
+            end
+          end
+        end
+      end
+    end
   end
 end
