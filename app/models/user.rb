d25c18a6b3622d18a9d3ffeaf1b58909736a3cc4
Laura Court <laura.mcourt@gmail.com>
2021-10-14 17:21:20 -0400

    Add relationships
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
   validates :password_confirmation, presence: { on: :create }
+
+  has_many :invitations, dependent: :destroy
+  has_many :parties, through: :invitations
 end
