047b1f8f860d365bda85c45e94055d16919b22d4
Laura Court <laura.mcourt@gmail.com>
2021-10-14 20:05:26 -0400

    Add friendship relationships
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
 
   has_many :invitations, dependent: :destroy
   has_many :parties, through: :invitations
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+  def all_friends
+    friends + inverse_friends
+  end
 end
