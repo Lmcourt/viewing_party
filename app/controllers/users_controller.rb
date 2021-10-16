fc63ab1624e098fb3b4c49971b0dc881f39cacc8
Tanner Dale <tanner@tannerdale.com>
2021-10-16 14:47:53 -0500

    Make require user an inherited before action, add skips where needed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user, except: %i[new create]
+  skip_before_action :require_user, only: %i[new create]
 
   def new
     @user = User.new
