e5e0354e3ed9f0b3f78a370bbc864fadf7f04ba7
Laura Court <laura.mcourt@gmail.com>
2021-10-19 20:09:13 -0400

    Add poster
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- app/poros/movies_poro.rb
+++ app/poros/movies_poro.rb
@@ -1,9 +1,16 @@
 class MoviesPoro
-  attr_reader :title, :vote_average, :id
+  attr_reader :title, :vote_average, :id, :poster
 
   def initialize(data)
     @id = data[:id]
     @title = data[:title]
     @vote_average = data[:vote_average]
+    @poster = poster_path(data[:poster_path])
+  end
+
+  private
+
+  def poster_path(url)
+    "https://image.tmdb.org/t/p/w500#{url}?api_key=#{ENV['movie_api_key']}"
   end
 end
