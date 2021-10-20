2f60c5457f92a7dd752f8ec9420dda47f410b022
Laura Court <laura.mcourt@gmail.com>
2021-10-19 22:39:54 -0400

    Add user avatar faker
    Co-authored-by: Tanner Dale 
    <77412134+TannerDale@users.noreply.github.com>
    
    Co-Authored-By: Tanner Dale <TannerDale@users.noreply.github.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { 'secretpasswordshhhh' }
     password_confirmation { 'secretpasswordshhhh' }
+    avatar { 'https://randomfox.ca/images/5.jpg' }
   end
 end
